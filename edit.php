bcee52cc0485beac326a8725b8c5a538d2a6a69c
= <paulobonifacio2005@gmail.com>
2020-07-13 12:12:50 +0200

    edit.php und Crash schutz hinzugefügt

---
+++ edit.php
@@ -0,0 +1,4 @@
+<?php 
+echo "Ermöglicht Benutzern Problem zuzuweisen und denjenigen, der Probleme eingereicht hat, diese zu editiern <br>";
+echo "Soll Ermöglichen, dass Problmeme als gelöst gekennzeichnet werden";
+?>
--- showissue.php
+++ showissue.php
@@ -1,21 +1,37 @@
 <?php 
 require_once("lib/session.php");
+require_once("generic_html.php");
 rh_session();
+generic_header();
 $id = $_GET['id'];
 
 $res = mysqli_query($mysql, "SELECT * FROM issues WHERE id = $id");
-$issue = mysqli_fetch_assoc($res);
-$room_id = $issue['room_id'];
-$res = mysqli_query($mysql, "SELECT * FROM rooms WHERE id = $room_id");
-$room = mysqli_fetch_assoc($res);
-//$res = mysqli_query($mysql, "SELECT * FROM");
-$item_id = $issue['item_id'];
-$res = mysqli_query($mysql, "SELECT * FROM items WHERE id = $item_id");
-$item = mysqli_fetch_assoc($res);
-echo "Problem mit Gerät:  " . $item['name'] . " in Raum: " . $room['name'];
-echo "<hr>";
-echo "<p> Beschreibung: " . $issue['comment']. "</p>";
 
+$issue = mysqli_fetch_assoc($res);
+if ($issue !== NULL) {
+    $room_id = $issue['room_id'];
+    $res = mysqli_query($mysql, "SELECT * FROM rooms WHERE id = $room_id");
+    $room = mysqli_fetch_assoc($res);
+    //$res = mysqli_query($mysql, "SELECT * FROM");
+    $item_id = $issue['item_id'];
+    $res = mysqli_query($mysql, "SELECT * FROM items WHERE id = $item_id");
+    $item = mysqli_fetch_assoc($res);
+    echo "Problem mit Gerät:  " . $item['name'] . " in Raum: " . $room['name'];
+    echo "<hr>";
+    echo "<p> Beschreibung: " . $issue['comment']. "</p>";
+    if ($issue['assignee_id' !== -1]) {
+        $res = mysqli_query($mysql, "SELECT * FROM users WHERE id = " . $issue['assignee_id']);
+        $assignee = mysqli_fetch_assoc($res);
+        echo "Zugewiesen: " . $assignee['name'] . "<br>";
+    }
+    else {
+    echo "Es wurde noch niemand zugewiesen <br>";  
+    echo "<a href=\"edit.php?id=$id\">Problem bearbeiten</a>"; 
+    }
+}
+else {
+    echo "Es wurde kein Problem mit der ID $id gefunden. Das ist ein Problem";
+}
 function Box($title, $body) { 
 echo '<div class="box"><div class=box-"title">';
 echo $title;

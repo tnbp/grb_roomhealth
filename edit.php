f7e0a19e72b20d7f3bed39b124e627c91f2f0bb6
= <paulobonifacio2005@gmail.com>
2020-07-13 12:14:01 +0200

    Removed edit.php

--- edit.php
+++
@@ -1,4 +0,0 @@
-<?php 
-echo "Ermöglicht Benutzern Problem zuzuweisen und denjenigen, der Probleme eingereicht hat, diese zu editiern <br>";
-echo "Soll Ermöglichen, dass Problmeme als gelöst gekennzeichnet werden";
-?>

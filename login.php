b9fdc367bf17f04a20461041878e6f6c76bf2e35
Ian Schwarz <ian@d.atacube.de>
2020-08-18 07:52:13 +0200

    fix invalid url after login if error is not the first parameter

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ $res = mysqli_query($mysql, "SELECT * FROM users WHERE login = '" . mysqli_real_
 $rc = mysqli_num_rows($res);
 
 $nexturi = urldecode($_POST['nexturi']);
-$nexturi = preg_replace("/[?&]error=[^&]*/", "", $nexturi);
+$nexturi = preg_replace("/([?&])error=[^&]+(&|\$)/", "\$1", $nexturi);
 
 if (verify_login($_POST['login'], $_POST['pwd']) === true) {
 	redirect($nexturi);
